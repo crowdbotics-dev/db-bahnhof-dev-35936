1a23005ae8acc0574f2dbc971e28c57e5ab6e651
Crowdbotics <engineering@crowdbotics.com>
2022-09-28 14:11:07 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,11 @@
+from django.conf import settings
 from django.db import models
+
+
+class Location(models.Model):
+    "Generated Model"
+    lat = models.FloatField()
+    lng = models.FloatField()
+    name = models.CharField(
+        max_length=256,
+    )
